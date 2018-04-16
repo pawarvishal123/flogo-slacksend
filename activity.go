89fb3a805445f5a328f8ce76e02469244046b6b1
pawarvishal123 <pawarvishal123@gmail.com>
2018-04-16 14:48:39 +0530

    Initial code

---
+++ activity.go
@@ -0,0 +1,66 @@
+package slacksend
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/nlopes/slack"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+// log is the default package logger
+var flogoLogger = logger.GetLogger("activity-tibco-slacksend")
+
+// MyActivity is a stub for your Activity implementation
+type SlackSendActivity struct {
+	metadata        *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	flogoLogger.Debugf("SlackSend NewActivity")
+	slackSendActivity := &SlackSendActivity{metadata: metadata}
+	return slackSendActivity
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *SlackSendActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *SlackSendActivity) Eval(context activity.Context) (done bool, err error) {
+		flogoLogger.Debugf("SlackSend Eval")
+	
+		accesstoken := context.GetInput("AccessToken")
+		message := context.GetInput("Message")
+		channel := context.GetInput("Channel")
+		
+		api := slack.New(accesstoken)
+		params := slack.PostMessageParameters{}
+		/*attachment := slack.Attachment{
+		Pretext: "Flogo",
+		Text:    message,
+		// Uncomment the following part to send a field too
+		/*
+			Fields: []slack.AttachmentField{
+				slack.AttachmentField{
+					Title: "a",
+					Value: "no",
+				},
+			},
+		*/
+		//}
+		//params.Attachments = []slack.Attachment{attachment}
+		channelID, timestamp, err := api.PostMessage(channel, message, params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		context.SetOutput("timestamp", timestamp)
+		flogoLogger.Debugf("Message successfully sent to channel %s at %s", channelID, timestamp)
+		return true, nil
+	}
+	return false, fmt.Errorf("SlackSend called without a message to publish")
+}
---
+++ activity_test.go
@@ -0,0 +1,57 @@
+package slacksend
+
+import (
+	"io/ioutil"
+	"log"
+	"testing"
+
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+)
+
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+
+	return activityMetadata
+}
+
+func TestCreate(t *testing.T) {
+	act := NewActivity(getActivityMetadata())
+	if act == nil {
+		t.Error("Activity Not Created")
+		t.Fail()
+		return
+	}
+	log.Println("TestCreate successfull")
+}
+
+func TestPlain(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Failed()
+			t.Errorf("panic during execution: %v", r)
+		}
+	}()
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//setup attrs
+	tc.SetInput("AccessToken", "abc")
+	tc.SetInput("Channel", "general")
+	tc.SetInput("Message", "Hello from Flogo")
+	act.Eval(tc)
+	log.Printf("TestEval successfull.  timestamp [%s] ", tc.GetOutput("timestamp"))
+}
